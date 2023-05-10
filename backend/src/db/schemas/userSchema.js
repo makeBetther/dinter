1c931865be5b4cb14dd873c69c486fe0f4857f67
paikpaik <woduf9311@gmail.com>
2023-05-10 18:01:03 +0900

    crt: 유저스키마 추가

---
+++ backend/src/db/schemas/userSchema.js
@@ -0,0 +1,91 @@
+const UserSchema = new Schema({
+  userId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  dateOfBirth: {
+    type: String,
+    required: true,
+  },
+  gender: {
+    type: String,
+    required: true,
+    enum: ["male", "female"]
+  },
+  partnerGender: {
+    type: String,
+    required: true,
+    enum: ["male", "female", "both"]
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  relationship: {
+    type: String,
+    required: true,
+    enum: ["serious", "casual","serious80", "casual80", "neighborhood", "NotSureYet"]
+  },
+관심사
+5개만 [배열]
+enum[30개]
+  photos: [
+    {
+      filename: {
+        type: String
+      },
+      URL: {
+        type: String
+      }
+    },
+    {
+      filename: {
+        type: String
+      },
+      URL: {
+        type: String
+      }
+    },
+    {
+      filename: {
+        type: String
+      },
+      URL: {
+        type: String
+      }
+    },
+    {
+      filename: {
+        type: String
+      },
+      URL: {
+        type: String
+      }
+    },
+    {
+      filename: {
+        type: String
+      },
+      URL: {
+        type: String
+      }
+    },
+    {
+      filename: {
+        type: String
+      },
+      URL: {
+        type: String
+      }
+    }
+  ]
+});
