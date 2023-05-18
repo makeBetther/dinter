dd68595787cab4d3ba3b81409fdaac0b11870abd
savazyggg <younji0715@gmail.com>
2023-05-19 05:26:50 +0900

    FEAT: cards page update

--- src/componentss/card/Cards.jsx
+++ src/componentss/card/Cards.jsx
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+const API = "http://localhost:3004/users";
 
 const Cards = () => {
-  return <div>card</div>;
+  const [users, setUsers] = useState([]);
+
+  async function fetchUserApi() {
+    const response = await fetch(API);
+    const body = await response.json();
+    setUsers(body);
+  }
+  console.log(users);
+  useEffect(() => {
+    fetchUserApi();
+  }, []);
+
+  return (
+    <div>
+      {users.map((user) => {
+        return (
+          <ul key={user.id}>
+            <li>{user.name}</li>
+            <img src={user.photos.map((pic) => pic.URL)}></img>
+            <li>{user.gender}</li>
+            <li>{user.relationship}</li>
+          </ul>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Cards;
